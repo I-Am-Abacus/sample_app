cbca3e5a351df1476fda0c1be40ec1564f24a797
I-Am-Abacus <martinrpayneuk+git@gmail.com>
2014-04-29 18:05:33 +0100

    Finish user edit, update, index, and destroy actions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'support/utilities'
 
 describe 'Static pages' do
 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,18 @@
 include ApplicationHelper
 require 'spec_helper'
 
+def spec_sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+  else
+    visit signin_path
+    valid_signin(user)
+  end
+end
+
 def valid_signin(user)
   fill_in 'Email',      with: user.email
   fill_in 'Password',   with: user.password
@@ -16,6 +28,12 @@ end
 RSpec::Matchers.define :have_success_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_notice_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-notice', text: message)
   end
 end
 
